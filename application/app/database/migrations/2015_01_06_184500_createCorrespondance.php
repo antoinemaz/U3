f0790d3ed1ee1ca6d6e9b8751510b8977e6709ba
ptimat <mat2leuleu@hotmail.fr>
2015-01-06 19:19:11 +0100

    Page Connexion / Modifs OrdreMigra.txt / Ajout table / Modif table utilisateur

---
+++ application/app/database/migrations/2015_01_06_184500_createCorrespondance.php
@@ -0,0 +1,24 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class CreateCorrespondance extends Migration {
+	
+	public function up()
+	{
+		Schema::create('correspondances', function($table)
+		{
+			/*Creation d'un champs de type autoincrement et en clé primaire*/
+			$table->increments('id')->unsigned();
+			$table->string('iduser');
+			$table->string('filieres_resp');
+			$table->string('annees_resp');
+			$table->foreign('iduser')->references('id')->on('utilisateurs');
+			/* created at et updated at sont créé à l'aide de timestamp() */
+			$table->timestamps();
+		});
+	}
+	public function down()
+	{
+		Schema::drop('correspondances');
+	}
+}
